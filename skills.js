eccc67b805c5fa9aab23b98290b598988ed80e1c
Tomasz Sielski <95984424+TomaszSielski@users.noreply.github.com>
2023-08-02 21:51:47 +0000

    next1 suggestions

--- skills.js
+++ skills.js
@@ -4,3 +4,6 @@ function calaculateNumbers(var1, var2){
 function recalculateNumbers(var1, var2){
     console.log(var1-var2);
 }
+function calculateNumbers(var1, var2){
+    console.log(var1*var2);
+}
